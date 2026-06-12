ad4c91c57a69cc5c6d0701d14582e140a1c3baa5
TheisenR <theisen.reddy@gmail.com>
2026-06-12 15:01:21 -0700

    Bug Fixes

--- server/main.js
+++ server/main.js
@@ -1,9 +1,28 @@
 import { Meteor } from 'meteor/meteor';
 import { Accounts } from 'meteor/accounts-base';
+import 'meteor/accounts-password';
 import { check } from 'meteor/check';
 
 Meteor.startup(() => {
   console.log('Meteor server started');
+
+  const defaultEmail = 'john@students.kpu.ca';
+  const defaultPassword = 'Password123!';
+
+  const user = Accounts.findUserByEmail(defaultEmail);
+  if (!user) {
+    const userId = Accounts.createUser({
+      email: defaultEmail,
+      password: defaultPassword,
+      profile: {
+        name: 'Test Student'
+      }
+    });
+    console.log(`Created default login user: ${defaultEmail} (${userId})`);
+  } else if (typeof Accounts.setPassword === 'function') {
+    Accounts.setPassword(user._id, defaultPassword);
+    console.log(`Reset password for existing user: ${defaultEmail}`);
+  }
 });
 
 Meteor.methods({
